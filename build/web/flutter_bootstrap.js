e9b4f97b00c2a19aed34ec96a9a14d9ba41f74e3
Nguyễn Hồng Ánh <98610210+NguyenHongAnh0812@users.noreply.github.com>
2025-07-10 10:36:05 +0700

    fix 1

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1448409708"
+    serviceWorkerVersion: "2148537084"
   }
 });
